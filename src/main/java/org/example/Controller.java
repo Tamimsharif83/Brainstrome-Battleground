7ee6081fddba7e12da4f8383a988abc6b1a1a21b
Tamim Sharif <202314083@student.mist.ac.bd>
2025-02-09 10:07:29 +0600

    UI developed

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.util.Duration;
+
+import javax.swing.text.html.ImageView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            // Load the FXML file from the correct resource folder
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/fxml/gameface.fxml"));
+
+            // Create a scene using the FXML file
+            // Scene scene = new Scene(fxmlLoader.load(), 824, 530);
+            Parent root = fxmlLoader.load();// Adjusted size according to FXML
+            Scene scene = new Scene(root);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // Print error information for debugging purposes
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(); // Launch the JavaFX application
+    }
+}
