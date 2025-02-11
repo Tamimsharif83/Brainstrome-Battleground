7f3c96e4b8874f9a26c55a3d51685dceb3273128
Tamim Sharif <202314083@student.mist.ac.bd>
2025-02-11 14:24:33 +0600

    Exit button of gameface.fxml handeling code added

--- src/main/java/org/example/GamefaceController.java
+++ src/main/java/org/example/GamefaceController.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,12 +9,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import java.io.IOException;
 
-public class GamefaceController{
+public class GamefaceController {
 
-    public void handleSettingsButton(ActionEvent event) throws IOException {
+    @FXML // Add this annotation to allow FXML to recognize the method
+    private void handleSettingsButton(ActionEvent event) throws IOException {
         // Load the settings page FXML
         Parent settingsRoot = FXMLLoader.load(getClass().getResource("/fxml/settingspage.fxml"));
-
 
         // Get the current stage from the event
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -23,5 +24,15 @@ public class GamefaceController{
 
         // Show the stage
         stage.show();
+    }
+
+    @FXML
+    private void handleExit(ActionEvent event) {
+        // Get the current stage and close it
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+
+        // OR, if you want to exit the entire application
+        System.exit(0);
     }
 }
